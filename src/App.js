c7e752b0cc3d168e7dee46727dcb183fc0d80292
chrisrainespro <chris.raines@bethel.com>
2023-09-13 12:34:49 -0400

    created single admin component and started tables

--- src/App.js
+++ src/App.js
@@ -2,11 +2,9 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import VideoList from "./VideoList";
-import Video from "./Video";
-import Admin from "./admin";
+import Admin from "./Admin";
 import SeriesList from "./SeriesList";
-import AdminVideoList from "./AdminVideoList";
-import AdminSeriesList from "./AdminSeriesList";
+
 
 
 function App() {
@@ -18,11 +16,7 @@ function App() {
         <Route path="videos/list/:categoryId" element={<VideoList />} />
         <Route path="series/list/:seriesId" element={<SeriesList /> } />
       </Route>
-      <Route path='/admin' element={<Home/>}>
-      <Route index element={<p>Please Select a Category from the Menu Above</p>}  />
-        <Route path="videos/list/:categoryId" element={<AdminVideoList />} />
-        <Route path="series/list/:seriesId" element={<AdminSeriesList /> } />
-      </Route>
+      <Route path='/admin' element={<Admin/>} />
     </Routes>
     </BrowserRouter>
   );
--- src/admin.js
+++ src/admin.js
@@ -1,5 +1,49 @@
-function Admin(){
-    return <></>
+import React, { useState, useEffect, useContext } from "react"
+import { Button, Table } from "react-bootstrap"
+
+
+export default function Admin(props){
+
+    return (
+        <>
+        <h1>Catagories</h1>
+        <Table>
+           <tr>
+                <th>Title</th>
+                <th>Contains Series</th>
+                <th>Edit</th>
+                <th>Delete</th>
+                
+            </tr>
+
+        </Table>
+        <Button>Add Category</Button>
+        <h1>Series</h1>
+        <Table>
+           <tr>
+                <th>Title</th>
+                <th>Category Name</th>
+                <th>Edit</th>
+                <th>Delete</th>
+            </tr>
+             
+        </Table>
+        <Button>Add Series</Button>
+        <h1>Videos</h1>
+        <Table>
+           <tr>
+                <th>Category Name</th>
+                <th>Series</th>
+                <th>Title</th>
+                <th>Password</th>
+                <th>Edit</th>
+                <th>Delete</th>
+            </tr>
+             
+        </Table>
+        <Button>Add Video</Button>
+        </>
+    )
+    
 }
 
-export default Admin
